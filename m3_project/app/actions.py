54f76cd2770d9b79de299a776dc56797ec8bd709
helloworldrussia <coolgrifers@gmail.com>
2022-06-06 16:26:21 +0300

    columns

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -6,18 +6,33 @@ from objectpack.actions import ObjectPack
 class UserPack(ObjectPack):
     model = User
     add_to_menu = True
+    can_delete = True
+
+    columns = [
+        {
+            'data_index': '__unicode__',
+            'header': u'username',
+        },
+        {
+            'data_index': 'email',
+            'header': u'email',
+        },
+    ]
 
 
 class ContentTypePack(ObjectPack):
     model = ContentType
     add_to_menu = True
+    can_delete = True
 
 
 class GroupPack(ObjectPack):
     model = Group
     add_to_menu = True
+    can_delete = True
 
 
 class PermissionPack(ObjectPack):
     model = Permission
     add_to_menu = True
+    can_delete = True
---
+++ m3_project/app/ui.py
@@ -0,0 +1,62 @@
+from objectpack.ui import BaseEditWindow, make_combo_box
+from m3_ext.ui import all_components as ext
+
+
+class UserAddWindow(BaseEditWindow):
+
+    def _init_components(self):
+        """
+        Здесь следует инициализировать компоненты окна и складывать их в
+        :attr:`self`.
+        """
+        super(UserAddWindow, self)._init_components()
+
+        self.field__username = ext.ExtStringField(
+            label=u'username',
+            name='username',
+            allow_blank=False,
+            anchor='100%')
+
+        self.field__first_name = ext.ExtStringField(
+            label=u'first name',
+            name='first_name',
+            allow_blank=False,
+            anchor='100%')
+
+        self.field__last_name = ext.ExtStringField(
+            label=u'last name',
+            name='last_name',
+            allow_blank=False,
+            anchor='100%')
+
+        self.field__last_name = ext.ExtStringField(
+            label=u'email',
+            name='email',
+            allow_blank=False,
+            anchor='100%')
+
+        self.field__birthday = ext.ExtDateField(
+            label=u'Дата рождения',
+            name='birthday',
+            anchor='100%')
+
+    def _do_layout(self):
+        """
+        Здесь размещаем компоненты в окне
+        """
+        super(PersonAddWindow, self)._do_layout()
+        self.form.items.extend((
+            self.field__name,
+            self.field__surname,
+            self.field__gender,
+            self.field__birthday,
+        ))
+
+    def set_params(self, params):
+        """
+        Установка параметров окна
+
+        :params: Словарь с параметрами, передается из пака
+        """
+        super(PersonAddWindow, self).set_params(params)
+        self.height = 'auto'
