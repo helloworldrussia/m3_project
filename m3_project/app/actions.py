35fdcb1bd3ff29fc410efe18455dfdbba13b257a
helloworldrussia <coolgrifers@gmail.com>
2022-06-06 16:48:27 +0300

    correct names v2 ))

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -42,10 +42,10 @@ class UserPack(ObjectPack):
     def prepare_row(self, obj, request, context):
 
         obj.is_active = '<div class="x-grid3-check-col%s"/>' % (
-            '-on' if obj.is_stuff is True else '')
+            '-on' if obj.is_active is True else '')
 
-        obj.is_stuff = '<div class="x-grid3-check-col%s"/>' % (
-            '-on' if obj.is_stuff is True else '')
+        obj.is_staff = '<div class="x-grid3-check-col%s"/>' % (
+            '-on' if obj.is_staff is True else '')
 
         return obj
 
