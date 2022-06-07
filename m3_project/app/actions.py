1c39a686073ff55cebc4ae2edc815ffd0b23c29c
helloworldrussia <coolgrifers@gmail.com>
2022-06-07 11:59:36 +0300

    ...

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -139,7 +139,7 @@ class GroupPack(ObjectPack):
             'header': u'permissions',
             'filter': {
                 'type': 'list',
-                'options': model.permissions.objects.all()
+                'options': Permission.objects.all()
             }
         },
     ]
